567bcf685fb6e889e01edb8d5e75145f539a109f
Pratik Kakade <Pratikkakade4618@gmail.com>
2026-04-09 09:36:24 +0530

    Update Naukri credentials and resume file paths

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
 # Configuration and constants used in the script. Update values as needed.
 
-USERNAME = "your_naukri_username"
-PASSWORD = "your_naukri_password"
-MOBILE = "your_naukri_phone"
-ORIGINAL_RESUME_PATH="your-resume-file-path/Resume.pdf"
-MODIFIED_RESUME_PATH="modified-resume-file-path/Resume.pdf"
+USERNAME = "Pratikkakade4618@gmail.com"
+PASSWORD = "Pratik@123"
+MOBILE = "7385904272"
+ORIGINAL_RESUME_PATH="./resume.pdf"
+MODIFIED_RESUME_PATH="./resume.pdf"
 NAUKRI_LOGIN_URL="https://www.naukri.com/nlogin/login"
 NAUKRI_PROFILE_URL="https://www.naukri.com/mnjuser/profile"
